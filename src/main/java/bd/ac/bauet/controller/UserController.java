ede0bb66fa76ad51f05f91d7af56fa616023db05
baseplate-admin <zombieahnaf@gmail.com>
2025-03-28 14:14:52 +0600

    Add

--- src/main/java/bd/ac/bauet/controller/UserController.java
+++ src/main/java/bd/ac/bauet/controller/UserController.java
@@ -1,17 +1,26 @@
 package bd.ac.bauet.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import bd.ac.bauet.model.User;
+
+import org.springframework.security.core.Authentication;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
 
     @GetMapping
-    public ResponseEntity<String> getUser() {
-        return ResponseEntity.ok("User details");
+    public ResponseEntity<?> getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        User user = (User) userDetails;
+        return ResponseEntity.ok(user);
     }
 
 }
